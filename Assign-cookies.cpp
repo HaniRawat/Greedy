abf780690dfff96ad0de0c8e9a52a4ef685e3b66
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-20 22:36:31 +0530

    solved

---
+++ Assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size(), i = 0, ans = 0, m = s.size(), j = 0;
+
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        while(i < n && j < m){
+            if(g[i] <= s[j]){
+                ans++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
