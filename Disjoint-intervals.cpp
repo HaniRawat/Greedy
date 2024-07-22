1a0048c5246564a23580de15db6b93c7f59580ed
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-22 21:39:17 +0530

    solved

---
+++ Disjoint-intervals.cpp
@@ -0,0 +1,18 @@
+bool compareEndPoints(vector<int>&a, vector<int>&b){
+    return a[1] < b[1];
+}
+
+int Solution::solve(vector<vector<int> > &A) {
+    sort(A.begin(), A.end(), compareEndPoints);
+    
+    int count = 1;
+    int lastEnd = A[0][1];
+    
+    for(int i = 1; i<A.size(); i++){
+        if(A[i][0] > lastEnd){
+            lastEnd = A[i][1];
+            count++;
+        }
+    }
+    return count;
+}
